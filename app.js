85d1d8aab27590e5dc8c3d279bb675a5cc8611f8
XxDrancerxX <idmdiazmora98@gmail.com>
2025-02-24 18:22:44 +0000

    asd

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
-const fromEuroToDollar = function(valueInEuro){
-      let valueInDollar = valueInEuro * 1.07;
-      return valueInDollar
+const fromEuroToDollar = function(euroUnit){
+      let dollarInEuro = euroUnit * 1.07; 
+      return dollarInEuro
 }
 
-const fromDollarToYen = function(dollar){
-      const yenAmount = dollar * (156.5/1.07);
-      return yenAmount;
+const fromDollarToYen = function(dollarUnit){
+      const yenInDollar = dollarUnit * (156.5/1.07);
+      return yenInDollar;
 }
 
-const fromYenToPound = function(yen){
-    const poundAmount = yen * (1 / 156.5 * 0.87);
-    return poundAmount;
+const fromYenToPound = function(yenUnit){
+    const poundInYen = yenUnit * (1 / 156.5 * 0.87);
+    return poundInYen;
 }
 
 module.exports = {fromDollarToYen,fromEuroToDollar,fromYenToPound};
 
-   // let oneEuroIs = {
-        //     "JPY": 156.5, // japan yen
-        //     "USD": 1.07, // us dollar
-        //     "GBP": 0.87, // british pound
-        // }
+   let oneEuroIs = {
+            "JPY": 156.5, // japan yen
+            "USD": 1.07, // us dollar
+            "GBP": 0.87, // british pound
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 test("converting 3.5 euros to dollars should return the correct value", function() {
     const { fromEuroToDollar } = require('./app.js');
-    const euroAmount = 3.5; // Define the euro amount
-    const expectedDollars = euroAmount * 1.07; // Calculate the expected value
-    expect(fromEuroToDollar(euroAmount)).toBe(expectedDollars); // Use the expected value in the assertion
+    const euroAmount = 3.5; 
+    const expectedDollars = euroAmount * 1.07; 
+    expect(fromEuroToDollar(euroAmount)).toBe(expectedDollars); 
 });
 
 test("converting 3.5 dollars to yens should return the correct value", function(){
@@ -21,8 +21,33 @@ test("converting 3.5 dollars to yens should return the correct value", function(
         
         });
 
-        // let oneEuroIs = {
-        //     "JPY": 156.5, // japan yen
-        //     "USD": 1.07, // us dollar
-        //     "GBP": 0.87, // british pound
-        // }
+        let oneEuroIs = {
+            "JPY": 156.5, // japan yen
+            "USD": 1.07, // us dollar
+            "GBP": 0.87, // british pound
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
